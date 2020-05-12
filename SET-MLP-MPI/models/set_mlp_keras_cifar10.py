ff5187a19086a5bd53440d6c8ff30fcb71d8aa78
Selima Curci <s.curci@student.tue.nl>
2020-05-12 15:09:15 +0200

    clean SET Keras

--- SET-MLP-MPI/models/set_mlp_keras_cifar10.py
+++ SET-MLP-MPI/models/set_mlp_keras_cifar10.py
@@ -40,6 +40,11 @@
 
 from __future__ import division
 from __future__ import print_function
+
+# Force Keras to use CPU
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
 from utils.load_data import *
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
@@ -66,36 +71,6 @@ class TestCallback(keras.callbacks.Callback):
         x, y = self.test_data
         loss, acc = self.model.evaluate(x, y, verbose=0)
         print('\nTesting loss: {}, acc: {}\n'.format(loss, acc))
-
-# Training settings
-parser = argparse.ArgumentParser(description='SET Parallel Training ')
-parser.add_argument('--batch-size', type=int, default=100, metavar='N',
-                    help='input batch size for training (default: 64)')
-parser.add_argument('--test-batch-size', type=int, default=3000, metavar='N',
-                    help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=200, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                    help='learning rate (default: 0.01)')
-parser.add_argument('--lr-rate-decay', type=float, default=0.0, metavar='LRD',
-                    help='learning rate decay (default: 0)')
-parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
-                    help='SGD momentum (default: 0.9)')
-parser.add_argument('--dropout-rate', type=float, default=0.3, metavar='D',
-                    help='Dropout rate')
-parser.add_argument('--weight-decay', type=float, default=0.00, metavar='W',
-                    help='Dropout rate')
-parser.add_argument('--epsilon', type=int, default=20, metavar='E',
-                    help='Sparsity level')
-parser.add_argument('--zeta', type=float, default=0.3, metavar='Z',
-                    help='It gives the percentage of unimportant connections which are removed and replaced with '
-                         'random ones after every epoch(in [0..1])')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--n-training-samples', type=int, default=50000, metavar='N',
-                    help='Number of training samples')
-parser.add_argument('--n-testing-samples', type=int, default=10000, metavar='N',
-                    help='Number of testing samples')
 
 
 class Constraint(object):
@@ -348,42 +323,8 @@ class SET_MLP_CIFAR10:
 
 if __name__ == '__main__':
 
-    args = parser.parse_args()
-
-    n_hidden_neurons = args.n_neurons
-    epsilon = args.epsilon
-    zeta = args.zeta
-    n_epochs = args.epochs
-    batch_size = args.batch_size
-    dropout_rate = args.dropout_rate
-    learning_rate = args.lr
-    momentum = args.momentum
-    weight_decay = args.weight_decay
-    n_processes = args.n_processes
-    n_training_samples = args.n_training_samples
-    n_testing_samples = args.n_testing_samples
-    learning_rate_decay = args.lr_rate_decay
-
-    # Prepare config object for the parameter server
-    config = {
-            'n_processes': n_processes,
-            'n_epochs': n_epochs,
-            'batch_size': batch_size,
-            'dropout_rate': dropout_rate,
-            'seed': 0,
-            'lr': learning_rate,
-            'lr_decay': learning_rate_decay,
-            'zeta': zeta,
-            'epsilon': epsilon,
-            'momentum': momentum,
-            'weight_decay': weight_decay,
-            'n_hidden_neurons': n_hidden_neurons,
-            'n_training_samples': n_training_samples,
-            'n_testing_samples': n_testing_samples,
-            'loss': 'mse'
-        }
     np.random.seed(0)
-    X_train, Y_train, X_test, Y_test = load_cifar10_data(args.n_training_samples, args.n_testing_samples)
+    X_train, Y_train, X_test, Y_test = load_cifar10_data(50000, 10000)
 
     # create and run a SET-MLP model on CIFAR10
     model=SET_MLP_CIFAR10()
@@ -391,9 +332,9 @@ if __name__ == '__main__':
     X_test = X_test.reshape(-1, 32, 32, 3)
 
     start_time = time.time()
-    model.fit(X_train, Y_train, X_test, Y_test, batch_size, testing=True,
-                save_filename="../Results/set_mlp_keras_cifar10_one_cpu" + str(n_training_samples) + "_training_samples_e" + str(
-                    epsilon) + "_rand" + str(0))
+    model.fit(X_train, Y_train, X_test, Y_test, 100, testing=True,
+                save_filename="../Results/set_mlp_keras_cifar10_one_cpu" + str(50000) + "_training_samples_e" + str(
+                    20) + "_rand" + str(0))
     step_time = time.time() - start_time
     print("\nTotal training time: ", step_time)
 
